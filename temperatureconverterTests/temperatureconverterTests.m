ac638e3e5f92fe1fa57cf4af5501645591dc8e6d
Jeremy Reyes <djreyes@gmail.com>
2013-10-05 21:45:45 -0700

    Initial Commit

---
+++ temperatureconverterTests/temperatureconverterTests.m
@@ -0,0 +1,34 @@
+//
+//  temperatureconverterTests.m
+//  temperatureconverterTests
+//
+//  Created by Jeremy Reyes on 10/5/13.
+//  Copyright (c) 2013 Jeremy Reyes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface temperatureconverterTests : XCTestCase
+
+@end
+
+@implementation temperatureconverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
